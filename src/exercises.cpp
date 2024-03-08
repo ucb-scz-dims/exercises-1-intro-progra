dc8b3ade1cb6461e3a8f0c8a7ae17e28175fb4c2
Jose Jesus <45214219+cpjosejesus@users.noreply.github.com>
2024-03-08 11:45:03 -0400

    aaaaaaaaaaaaaaaaaa

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 void exercise_1(string s1, string s2, string s3) {
   // TODO: YOUR CODE HERE
-  cout << "milk";
+  cout << "a";
 }
 
 void exercise_2(double A, double B, double C) {
