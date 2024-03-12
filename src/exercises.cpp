7bd32e7d28105a42b810e5129837f42698998b32
D4ri4na <159303073+D4ri4na@users.noreply.github.com>
2024-03-12 17:57:23 -0400

    eje 11,13

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -155,6 +155,24 @@ void exercise_12(string color1, int numb1,
 
 string exercise_13(int age, int years_of_experience) {
   // TODO: YOUR CODE HERE
+  cin>> age;
+  cin >> years_of_experience;
+  if (age>=18 && years_of_experience>=5)
+  {
+    cout<<"Senior project manager";
+  } 
+  else if (age>=18 && years_of_experience>=3)
+  {
+    cout<<"Project manager";
+  } 
+  else if (age>=18)
+  {
+    cout<<"Project coordinator";
+  } 
+  else
+  {
+    cout<<"Not eligible";
+  } 
   return "";
 }
 
