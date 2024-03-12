1f9673e5d5d32b60ace326a933fe0c2f1815a1d7
D4ri4na <159303073+D4ri4na@users.noreply.github.com>
2024-03-12 14:04:44 -0400

    prueba eje11

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -133,8 +133,18 @@ int exercise_10(int a, int b) {
 
 string exercise_11(int number) {
   // TODO: YOUR CODE HERE
+    cin>> number;
+    if (number==11235813)
+    {
+      cout<<"Se encontro Fibonacci";
+    }
+    else
+    { 
+      cout<<"Esto no es de Fibonacci";
+    } 
+
   return "";
-}
+  }
 
 void exercise_12(string color1, int numb1,
                  string color2, int numb2,
